d70c5d8716188fc3a1d6551e08355024657d82b7
Rusty B <mycoding.alert@gmail.com>
2015-04-10 23:57:50 -0500

    Convert 1 passing.
    Refactored away the asumption that input would be string array, allowing for normal Dictionary operation.  Left a constructor that takes array for this commit only (just in case).

--- BankOcrTests/UserStory1Tests.cs
+++ BankOcrTests/UserStory1Tests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 
 namespace BankOcrTests
 {
@@ -11,35 +12,43 @@ namespace BankOcrTests
         [TestMethod]
         public void ToArabic_Zero_Returns0()
         {
-            var c = new DigitConverter();
-            var zeroDigit = new string[3];
-            zeroDigit[0] = " _ ";
-            zeroDigit[1] = "| |";
-            zeroDigit[2] = "|_|";
+            var digit = " _ "
+                      + "| |"
+                      + "|_|";
+            Assert.AreEqual(0, DigitConverter.ToArabic(digit));
+        }
 
-            var zeroArabic = c.ToArabic(zeroDigit);
-            Assert.AreEqual(0, zeroArabic);
+        [TestMethod]
+        public void ToArabic_One_Returns1()
+        {
+            var digit = "   "
+                      + "  |"
+                      + "  |";
+            Assert.AreEqual(1, DigitConverter.ToArabic(digit));
         }
     }
 
-    public class DigitConverter
+    public static class DigitConverter
     {
-        private Dictionary<int, string[]> arabicDigitMap;
-
-        public DigitConverter()
+        private static Dictionary<string, int> digitArabicMap = new Dictionary<string, int>
         {
-            var digit = new string[3];
-            digit[0] = " _ ";
-            digit[1] = "| |";
-            digit[2] = "|_|";
+            {  " _ "
+             + "| |"
+             + "|_|", 0},
+            {  "   "
+             + "  |"
+             + "  |", 1}
 
-            arabicDigitMap = new Dictionary<int, string[]>();
-            this.arabicDigitMap.Add(0, digit);
+        };
+
+        public static int ToArabic(string digit)
+        {
+            return digitArabicMap[digit];
         }
 
-        public int ToArabic(string[] digit)
+        public static int ToArabic(string[] digit)
         {
-            return arabicDigitMap.FirstOrDefault(kv => kv.Value == digit).Key;
+            return ToArabic(string.Join("", digit));
         }
     }
 }
