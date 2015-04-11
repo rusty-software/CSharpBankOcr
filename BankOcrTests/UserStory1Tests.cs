6751c360e25a300f68c697d5800e73a4cedb1536
Rusty B <mycoding.alert@gmail.com>
2015-04-10 23:29:03 -0500

    Initial commit: * Console project * Test project with:   : Resource file with digits for first use case   : One passing test
    Not sure I like the idea of the reverse lookup; will revisit that after more tests

---
+++ BankOcrTests/UserStory1Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankOcrTests
+{
+    [TestClass]
+    public class UserStory1Tests
+    {
+        [TestMethod]
+        public void ToArabic_Zero_Returns0()
+        {
+            var c = new DigitConverter();
+            var zeroDigit = new string[3];
+            zeroDigit[0] = " _ ";
+            zeroDigit[1] = "| |";
+            zeroDigit[2] = "|_|";
+
+            var zeroArabic = c.ToArabic(zeroDigit);
+            Assert.AreEqual(0, zeroArabic);
+        }
+    }
+
+    public class DigitConverter
+    {
+        private Dictionary<int, string[]> arabicDigitMap;
+
+        public DigitConverter()
+        {
+            var digit = new string[3];
+            digit[0] = " _ ";
+            digit[1] = "| |";
+            digit[2] = "|_|";
+
+            arabicDigitMap = new Dictionary<int, string[]>();
+            this.arabicDigitMap.Add(0, digit);
+        }
+
+        public int ToArabic(string[] digit)
+        {
+            return arabicDigitMap.FirstOrDefault(kv => kv.Value == digit).Key;
+        }
+    }
+}
