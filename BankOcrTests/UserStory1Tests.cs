ee69e2df5d94499826bd7ae0d676d79ed3157105
Rusty B <mycoding.alert@gmail.com>
2015-04-11 00:11:37 -0500

    Finished out the digits tests.

--- BankOcrTests/UserStory1Tests.cs
+++ BankOcrTests/UserStory1Tests.cs
@@ -26,6 +26,78 @@ namespace BankOcrTests
                       + "  |";
             Assert.AreEqual(1, DigitConverter.ToArabic(digit));
         }
+
+        [TestMethod]
+        public void ToArabic_Two_Returns2()
+        {
+            var digit = " _ "
+                      + " _|"
+                      + "|_ ";
+            Assert.AreEqual(2, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Three_Returns3()
+        {
+            var digit = " _ "
+                      + " _|"
+                      + " _|";
+            Assert.AreEqual(3, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Four_Returns4()
+        {
+            var digit = "   "
+                      + "|_|"
+                      + "  |";
+            Assert.AreEqual(4, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Five_Returns5()
+        {
+            var digit = " _ "
+                      + "|_ "
+                      + " _|";
+            Assert.AreEqual(5, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Six_Returns6()
+        {
+            var digit = " _ "
+                      + "|_ "
+                      + "|_|";
+            Assert.AreEqual(6, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Seven_Returns7()
+        {
+            var digit = " _ "
+                      + "  |"
+                      + "  |";
+            Assert.AreEqual(7, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Eight_Returns8()
+        {
+            var digit = " _ "
+                      + "|_|"
+                      + "|_|";
+            Assert.AreEqual(8, DigitConverter.ToArabic(digit));
+        }
+
+        [TestMethod]
+        public void ToArabic_Nine_Returns9()
+        {
+            var digit = " _ "
+                      + "|_|"
+                      + " _|";
+            Assert.AreEqual(9, DigitConverter.ToArabic(digit));
+        }
     }
 
     public static class DigitConverter
@@ -37,18 +109,36 @@ namespace BankOcrTests
              + "|_|", 0},
             {  "   "
              + "  |"
-             + "  |", 1}
-
+             + "  |", 1},
+            {  " _ "
+             + " _|"
+             + "|_ ", 2},
+            {  " _ "
+             + " _|"
+             + " _|", 3},
+            {  "   "
+             + "|_|"
+             + "  |", 4},
+            {  " _ "
+             + "|_ "
+             + " _|", 5},
+            {  " _ "
+             + "|_ "
+             + "|_|", 6},
+            {  " _ "
+             + "  |"
+             + "  |", 7},
+            {  " _ "
+             + "|_|"
+             + "|_|", 8},
+            {  " _ "
+             + "|_|"
+             + " _|", 9},
         };
 
         public static int ToArabic(string digit)
         {
             return digitArabicMap[digit];
-        }
-
-        public static int ToArabic(string[] digit)
-        {
-            return ToArabic(string.Join("", digit));
         }
     }
 }
