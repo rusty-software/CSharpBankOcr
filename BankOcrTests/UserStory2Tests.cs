dd0fb2e48d2338044f0d9287690bb397547a4e38
Rusty B <mycoding.alert@gmail.com>
2015-04-12 06:55:41 -0500

    User Story 2 passing.
    I realize this is all one commit, but the implementation was small and quick.

---
+++ BankOcrTests/UserStory2Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BankOcrTests
+{
+    [TestClass]
+    public class UserStory2Tests
+    {
+        private AccountNumberValidator validator = new AccountNumberValidator();
+
+        [TestMethod]
+        public void IsValid_GoodAccountNumber_ReturnsTrue()
+        {
+            Assert.IsTrue(validator.IsValid("345882865"));
+        }
+
+        [TestMethod]
+        public void IsValid_BadAccountNumber_ReturnsFalse()
+        {
+            Assert.IsTrue(validator.IsValid("123456789"));
+        }
+    }
+
+    public class AccountNumberValidator
+    {
+        public bool IsValid(string accountNumber)
+        {
+            // clarity over brevity
+            var checksum = 0;
+            var numbers = accountNumber.Select(c => c.ToString()).ToArray();
+            for (var i = 0; i < 9; i++)
+            {
+                checksum += Convert.ToInt16(numbers[i]) * (9 - i);
+            }
+            checksum = checksum % 11;
+            return checksum == 0;
+        }
+    }
+}
