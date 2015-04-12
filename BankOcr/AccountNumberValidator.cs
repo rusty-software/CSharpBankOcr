556ad837caa0ffe1467f16c7d0b8f58b3cecc56f
Rusty B <mycoding.alert@gmail.com>
2015-04-12 06:58:46 -0500

    Refactoring validator into main project.

---
+++ BankOcr/AccountNumberValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOcr
+{
+    public class AccountNumberValidator
+    {
+        public bool IsValid(string accountNumber)
+        {
+            // clarity over brevity
+            var checksum = 0;
+            var numbers = accountNumber.Select(c => c.ToString()).ToArray();
+            for (var i = 0; i < 9; i++)
+            {
+                checksum += Convert.ToInt16(numbers[i]) * (9 - i);
+            }
+            checksum = checksum % 11;
+            return checksum == 0;
+        }
+    }
+}
--- BankOcrTests/UserStory2Tests.cs
+++ BankOcrTests/UserStory2Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using BankOcr;
 
 namespace BankOcrTests
 {
@@ -19,22 +20,6 @@ namespace BankOcrTests
         public void IsValid_BadAccountNumber_ReturnsFalse()
         {
             Assert.IsTrue(validator.IsValid("123456789"));
-        }
-    }
-
-    public class AccountNumberValidator
-    {
-        public bool IsValid(string accountNumber)
-        {
-            // clarity over brevity
-            var checksum = 0;
-            var numbers = accountNumber.Select(c => c.ToString()).ToArray();
-            for (var i = 0; i < 9; i++)
-            {
-                checksum += Convert.ToInt16(numbers[i]) * (9 - i);
-            }
-            checksum = checksum % 11;
-            return checksum == 0;
         }
     }
 }
