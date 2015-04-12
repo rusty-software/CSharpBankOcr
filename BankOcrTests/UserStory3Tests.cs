074a71a9dd8c6830cf1c6a59516e5e37d279d5f1
Rusty B <mycoding.alert@gmail.com>
2015-04-12 07:04:50 -0500

    Skeleton for User Story 3 tests

---
+++ BankOcrTests/UserStory3Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankOcrTests
+{
+    [TestClass]
+    public class UserStory3Tests
+    {
+        [TestMethod]
+        public void Validator_BadCheckSum_ReturnsERR()
+        {
+            throw new NotImplementedException("Implement me!");
+
+        }
+
+        [TestMethod]
+        public void Validator_BadDigit_ReturnsILL()
+        {
+            throw new NotImplementedException("Implement me!");
+
+        }
+
+        [TestMethod]
+        public void Validator_OK_ReturnsGoodSignal()
+        {
+            throw new NotImplementedException("Implement me!");
+
+        }
+    }
+}
